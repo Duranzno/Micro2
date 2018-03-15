38a712b328b02602c6158566ea6cc96b5868ea15
Alejandro <aledurax@gmail.com>
2018-03-14 22:50:19 -0400

    Fixed stuff

--- Lab1/Barras.c
+++ Lab1/Barras.c
@@ -10,12 +10,24 @@ const int BARS_H_LOCATIONS[5]={0,1,2,3,4}
 typedef enum Bars{Int0,Int1,Int2,Int3,Int4};
 
 //Las barras se usan calculando la posicion X mas a la izquierda, todos tienen un alto y un ancho definidos
-void barras(Bars bar,int bar_counter) {
-     switch(bars){
+void barras(Bars interrupcion,int bar_counter) {
+     switch(interrupcion){
      	case Int0:
      	//Int0 tiene una posicion especifica constante asignada aqui, se toma 0 como ejemplo
      	// void Glcd_Box(unsigned short x_upper_left, unsigned short y_upper_left, unsigned short x_bottom_right, unsigned short y_bottom_right, unsigned short color);
-	     	bar_drawer(x_bars_locations[Int0],number_of_bars)
+	     	bar_drawer(BARS_H_LOCATIONS[Int0],bar_counter)
+    		break;
+    	case Int1:
+     		bar_drawer(BARS_H_LOCATIONS[Int1],bar_counter)
+    		break;
+    	case Int2:
+     		bar_drawer(BARS_H_LOCATIONS[Int2],bar_counter)
+    		break;
+    	case Int3:
+     		bar_drawer(BARS_H_LOCATIONS[Int3],bar_counter)
+    		break;
+    	case Int4:
+     		bar_drawer(BARS_H_LOCATIONS[Int4],bar_counter)
     		break;
      	default:
      	break;
