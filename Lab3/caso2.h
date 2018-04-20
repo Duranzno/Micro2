6db567049ca793040332b3d8e93841e7383b0d06
eabrito7 <eabrito@estudiante.unexpo.edu.ve>
2018-04-20 17:39:44 -0430

    cambios en modo de trabajo caso 2

--- Lab3/caso2.h
+++ Lab3/caso2.h
@@ -16,7 +16,7 @@ void config_cuadratura () {
   QEI1LECL=2335;
   IEC3bits.QEI1IE=1; //habilita interrupcion de Cuadratura
   IFS3bits.QEI1IF=0; //limpia la bandera de interrupcion
-  QEI1CONbits.PIMOD=2; // el contador se inicializa con el valor del registro QEI1IC
+  QEI1CONbits.PIMOD=1; // el contador se inicializa con el valor del registro QEI1IC
   QEI1ICH=valor_ini_h;
   QEI1ICL=valor_ini_l;
   QEI1STATbits.PCHEQIEN=1; //interrupcion por comparacion de cuadratura
@@ -41,9 +41,10 @@ void caso2 () {
     else if(QEI1CONbits.PIMOD==2){glcd_write_text(strcat("2","pimod"),2,0,1) ;delay_ms(500); glcd_write_text("         ",0,0,1);}
   //debido a que POSCNT cuenta con dos registros de 16bits,
   //los unimos en una variable para calcular la distancia
-  valor_actual=POS1CNTL;
   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  valor_actual=(valor_actual>>16)+POS1HLD;
+    while (1) {
+  valor_actual=POS1CNTH;
+  valor_actual=(valor_actual<<16)+POS1CNTL;
   recorrido=valor_actual;
   recorrido=recorrido*2.35619; //se divide el poscnt entre 4
   //luego se multiplica por (2.pi.r)/4
@@ -52,4 +53,5 @@ void caso2 () {
   if(valor_anterior<valor_actual) Glcd_Write_Text("derecha ", 1, 2, 1);
   else if(valor_anterior>valor_actual) Glcd_Write_Text("izquierda", 1, 2, 1);
   valor_anterior=valor_actual;
+     }
 }
