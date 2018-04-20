b9fc1b1850090de0b71b2740f3660a281938e1d1
eabrito7 <eabrito@estudiante.unexpo.edu.ve>
2018-04-20 17:50:41 -0430

    cambios en poscnt

--- Lab3/Lab3.c
+++ Lab3/Lab3.c
@@ -23,7 +23,9 @@ void int_captura() org 0x88 {
             inter_menor();// animacion de menor a 4500 cm
         IFS3bits.QEI1IF=0;
         if(QEI1statbits.IDXIEN==1)
-           {inttostr(QEI1CONbits.PIMOD,txt);glcd_write_text("_INDEX",0,0,1) ;delay_ms(500); glcd_write_text("             ",0,0,1);
+           {POS1CNTH=0;
+             POS1CNTL=2122;
+            inttostr(QEI1CONbits.PIMOD,txt);glcd_write_text("_INDEX",0,0,1) ;delay_ms(500); glcd_write_text("             ",0,0,1);
   }
 }
 
