827f7e98403072d32f5a94d8740267da99a3feaf
eabrito7 <eabrito@estudiante.unexpo.edu.ve>
2018-04-20 07:27:24 -0430

    nada importante

--- Lab3/caso2.h
+++ Lab3/caso2.h
@@ -1,3 +1,9 @@
-// 2.2 Visualizar la distancia recorrida, numero de vueltas y sentido del QAD.
-// 	*La primera medida será a partir de los 5000 cm después de activar un pulsador al pin INDEX. Trabaje con los modos de inicialización de POSCNT del QEI
-// 	*Si la distancia sobrepasa el 10% por encima o por debajo de su valor inicial debe visualizar una animación referida a esta situación por 5 segundos, utilice el modo comparación del QEI 
+﻿// 2.2 Visualizar la distancia recorrida, numero de vueltas y sentido del QAD.
+// 	*La primera medida será a partir de los 5000 cm después de activar un pulsador al pin INDEX. 
+Trabaje con los modos de inicialización de POSCNT del QEI
+// 	*Si la distancia sobrepasa el 10% por encima o por debajo de su valor inicial debe visualizar una animación referida a esta situación por 5 segundos, utilice el modo comparación del QEI
+
+
+
+
+receptor 100k  
