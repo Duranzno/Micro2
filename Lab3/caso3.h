79baac84276e1e3860305bb600478fc757e6d00d
eabrito7 <eabrito@estudiante.unexpo.edu.ve>
2018-04-21 23:21:46 -0430

    adc arreglado, muestra voltajes en pantalla bien (menos valores menores a 1V) comentado libreria caso 3 variables decimales (se usa para convertir float a  3 decimales) variable pote1 creada, ya que no podia usar pot1 por estar caso 3 comentado

--- Lab3/caso3.h
+++ Lab3/caso3.h
@@ -12,26 +12,26 @@ void autolog(char *message,int value){
         glcd_write_text(strcat(txt2,message),0,7,1);delay_ms(500);glcd_write_text("                     ",0,7,1);
 }
 int xtoy(unsigned short x){
-	unsigned short center_x,r=30,center_y=64;
-	if(x<=64){center_x=32;}
-	else{center_x=96;}
-    int y=floor(center_y-sqrt(pow(r,2)-pow(x-center_x,2)));
+        unsigned short center_x,r=30,center_y=64;
+        if(x<=64){center_x=32;}
+        else{center_x=96;}
+   // int y=floor(center_y-sqrt(pow(r,2)-pow(x-center_x,2)));
     // autolog("r^2",ceil(pow(r,2)));
     // autolog("(x-cx)^2",pow(x-center_x,2));
     // autolog("sqrt(r^2-(x-cx)^2)",sqrt(pow(r,2)-pow(x-center_x,2)));
     // autolog("r^2",center_y-sqrt(pow(r,2)-pow(x-center_x,2)));
-	return y;
+        return y;
 }
 int xtoy2(unsigned short x){
-	unsigned short center_x,r=5,center_y=64;
-	if(x<=64){center_x=32;}
-	else{center_x=96;}
+        unsigned short center_x,r=5,center_y=64;
+        if(x<=64){center_x=32;}
+        else{center_x=96;}
     int y=floor(center_y-sqrt(pow(r,2)-pow(x-center_x,2)));
     // autolog("r^2",ceil(pow(r,2)));
     // autolog("(x-cx)^2",pow(x-center_x,2));
     // autolog("sqrt(r^2-(x-cx)^2)",sqrt(pow(r,2)-pow(x-center_x,2)));
     // autolog("r^2",center_y-sqrt(pow(r,2)-pow(x-center_x,2)));
-	return y;
+        return y;
 }
 void glcd_draw_pointer(unsigned short x,unsigned short center_x){
         //x_medida va de 0->60
@@ -39,30 +39,30 @@ void glcd_draw_pointer(unsigned short x,unsigned short center_x){
         Glcd_Line(center_x,64,x,xtoy(x,center_x,64),1);
 }
 void glcd_draw_subdiv(unsigned short startx,int page){
-	glcd_write_text("0K",xtoy2(startx),page,1);
-	startx+=12;
-	glcd_circle_fill(startx,xtoy2(startx),2,1);
-	glcd_write_text("1K",xtoy2(startx),page,1);
-	startx+=12;
-	glcd_circle_fill(startx,xtoy2(startx),2,1);
-	glcd_write_text("2K",xtoy2(startx),page,1);
-	startx+=12;
-	glcd_circle_fill(startx,xtoy2(startx),2,1);
-	glcd_write_text("3K",xtoy2(startx),page,1);
-	startx+=12;
-	glcd_circle_fill(startx,xtoy2(startx),2,1);
-	glcd_write_text("4K",xtoy2(startx),page,1);
-	startx+=12;
-	glcd_circle_fill(startx,xtoy2(startx),2,1);
-	glcd_write_text("5K",xtoy2(startx),page,1);
+        glcd_write_text("0K",xtoy2(startx),page,1);
+        startx+=12;
+        glcd_circle_fill(startx,xtoy2(startx),2,1);
+        glcd_write_text("1K",xtoy2(startx),page,1);
+        startx+=12;
+        glcd_circle_fill(startx,xtoy2(startx),2,1);
+        glcd_write_text("2K",xtoy2(startx),page,1);
+        startx+=12;
+        glcd_circle_fill(startx,xtoy2(startx),2,1);
+        glcd_write_text("3K",xtoy2(startx),page,1);
+        startx+=12;
+        glcd_circle_fill(startx,xtoy2(startx),2,1);
+        glcd_write_text("4K",xtoy2(startx),page,1);
+        startx+=12;
+        glcd_circle_fill(startx,xtoy2(startx),2,1);
+        glcd_write_text("5K",xtoy2(startx),page,1);
 }
 void glcd_draw_semicircles(int x){
-	glcd_circle(x,64,30,1);//nalga 1
-	glcd_draw_subdiv(x,2);
+        glcd_circle(x,64,30,1);//nalga 1
+        glcd_draw_subdiv(x,2);
 }
 
 void caso_3_impl_main(){
-	while(keydata!=ESC){
+        while(keydata!=ESC){
         clean_PS2();
         caso_3();
         Ps2_Key_Read(&keydata, &special, &down);
@@ -70,15 +70,15 @@ void caso_3_impl_main(){
     glcd_fill(0);
 }
 void caso_3(){
-	pot1=pot2;//PORQUE POR AHORA SOLO ESTA IMPLEMENTADO UN POTENCIOMETRO
-	glcd_draw_semicircles(32);
-	// glcd_draw_semicircles(96);
-	char txt2[15];
-	inttostr(pot1,txt2);glcd_write_text(strcat(pot1,message),0,0,1);	
-	glcd_draw_pointer(pot1+2,32)
+        pot1=pot2;//PORQUE POR AHORA SOLO ESTA IMPLEMENTADO UN POTENCIOMETRO
+        glcd_draw_semicircles(32);
+        // glcd_draw_semicircles(96);
+        char txt2[15];
+        inttostr(pot1,txt2);glcd_write_text(strcat(pot1,message),0,0,1);        
+        glcd_draw_pointer(pot1+2,32)
 }
 void caso_3_tests(){
-	int x;     char txt2[15]; int y;
+        int x;     char txt2[15]; int y;
     glcd_fill(0);
 
     y=xtoy(0+2,32,64);
