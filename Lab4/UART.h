745f2a12495ffdbd5838cb1ac1d2af9fb24d5da6
eabrito7 <eabrito@estudiante.unexpo.edu.ve>
2018-05-11 23:32:05 -0430

    arreglo uart

--- Lab4/UART.h
+++ Lab4/UART.h
@@ -3,12 +3,13 @@ U1MODEbits.STSEL = 0;// 1 Bit de parada
 U1MODEbits.PDSEL = 0;// No hay paridad, 8 bits de datos
 U1MODEbits.ABAUD = 0;// Auto-Bauddisabilitado
 U1MODEbits.BRGH = 0;// Modo de velocidad standard
-U1BRG = BRGVAL;// 9600 baudios
+U1BRG = 12;// 9600 baudios
 U1STAbits.UTXISEL0 = 0;// Interrupción al transmitir un caracter
 U1STAbits.UTXISEL1 = 0;
 IEC0bits.U1TXIE = 1;// habilita interrupción por transmisión
 U1MODEbits.UARTEN = 1;// Habilita UART
 UARTU1STAbits.UTXEN = 1;// Habilita transmisor del UART
+IEC0bits.U1RXIE=1;  // habilita interrupcion
 IFS0bits.U1TXIF=0;  // borra bandera de int
 IPC3bits.U1TXIP=5;  // nivel 5 de int
 }
@@ -20,9 +21,10 @@ U1MODEbits.STSEL = 0;// 1 bit de parada
 U1MODEbits.PDSEL = 0;// No paridad, 8-bits de datos
 U1MODEbits.ABAUD = 0;// Auto-Bauddesabilitado
 U1MODEbits.BRGH = 0;// velocidad estándar
-U1BRG = BRGVAL;// 9600 baudios
+U1BRG = 12;// 9600 baudios
 U1STAbits.URXISEL = 0;// Interrupción al recibir un carácter por RX ;
 U1MODEbits.UARTEN = 1;// Habilita UART
+IEC0bits.U1RXIE=1;  // habilita interrupcion
 IFS0bits.U1RXIF=0;  // borra bandera de int
 IPC2bits.U1RXIP=5;  // nivel 5 de int
 }
