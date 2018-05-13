4d04b6d104648cb83de71fd053ed16dcda0c9de5
Shuniorr <juniorlocool97@gmail.com>
2018-05-12 22:13:50 -0400

    g

--- Lab4/receptor uart/receptor.c
+++ Lab4/receptor uart/receptor.c
@@ -15,6 +15,8 @@ void main () {
         config_INT();
         config_LCD();
         U1UART_Init_Advanced(2400,2,1,1);
+        RPINR18bits.U1RXR=46; //U1RX
+        RPOR9bits.RP101R=1; //U1TX
         config_uart_rx ();
         Glcd_Write_TEXT("Laboratorio 4",31,0,1);
         while(1){        
