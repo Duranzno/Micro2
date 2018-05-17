3ff79aa0ba05faa119f8281d75d5c955aa381f71
Alejandro Duran <aledurax@gmail.com>
2018-05-17 16:33:07 -0400

    a

--- Lab4/RX/config_RX.h
+++ Lab4/RX/config_RX.h
@@ -58,7 +58,15 @@ void config_TMR2_ANIM () {
   IFS0bits.T2IF=0;
   IPC1bits.T2IP=7;
 }
-
+void INT_T05s()org 0x22{
+    T05s++;
+    if(T05s%2==0){T1s++;}
+    if(T05s==6){T05s=0;T1s=0;T1sCONbits.TON=0;}
+    if((caso==CASE_MAY||caso==CASE_MEN)&&T1s==3){T1s=0;}
+    else if((caso==CASE_PWM4||caso==CASE_PWM3)&&T1s==2){T1s=0;}
+    else if(caso==CASE_NULL&&T1s==2){T1s=0;}
+    selector_sprite(caso,T1s);
+}
 
 void InitMCU(){
   //CONFIGURACION DEL PLL PARA ALCANZAR UNA VELOCIDAD DE 30MHZ
